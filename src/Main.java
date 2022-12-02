a6d51d386e132b4ea204f0f56b27c482c0dbe50e
Mith <huyun1645@163.com>
2022-12-03 07:16:02 +0800

    Created file structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to job hunting!");
+        System.out.println("Welcome to job hunting service!");
     }
 }
