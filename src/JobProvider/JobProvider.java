e53bc91826d514901ddd22bb1dab9b2fd538e279
Alexis <517711766@qq.com>
2022-12-03 08:18:23 +0800

    Modified JobSeeker

--- src/JobProvider/JobProvider.java
+++ src/JobProvider/JobProvider.java
@@ -9,46 +9,5 @@ import java.util.List;
 
 public class JobProvider extends User {
 
-    private Boolean openToWork;
 
-    private List<Review> reviews;
-
-    private List<Invoice> invoices;
-
-    private List<Integer> assignedJobAd;
-
-    public JobProvider() {
-    }
-
-    public Boolean getOpenToWork() {
-        return openToWork;
-    }
-
-    public void setOpenToWork(Boolean openToWork) {
-        this.openToWork = openToWork;
-    }
-
-    public List<Review> getReviews() {
-        return reviews;
-    }
-
-    public void setReviews(List<Review> reviews) {
-        this.reviews = reviews;
-    }
-
-    public List<Invoice> getInvoices() {
-        return invoices;
-    }
-
-    public void setInvoices(List<Invoice> invoices) {
-        this.invoices = invoices;
-    }
-
-    public List<Integer> getAssignedJobAd() {
-        return assignedJobAd;
-    }
-
-    public void setAssignedJobAd(List<Integer> assignedJobAd) {
-        this.assignedJobAd = assignedJobAd;
-    }
 }
--- src/JobSeeker/JobSeeker.java
+++ src/JobSeeker/JobSeeker.java
@@ -1,6 +1,102 @@
 package JobSeeker;
 
+import Job.Review;
+import Payment.Invoice;
+import Server.Request;
 import User.User;
 
+import java.util.List;
+
 public class JobSeeker extends User {
+
+    private Boolean openToWork;
+
+    private List<Review> reviews;
+
+    private List<Invoice> invoices;
+
+    private List<Integer> assignedJobAd;
+
+    public String updatePreference(){
+        return null;
+    }
+
+    public String respondJob(Boolean respond){
+        return null;
+    }
+
+    public String collectPayment(){
+        return null;
+    }
+
+    private List<Integer> listCompletedJobs(){
+        return null;
+    }
+
+    private String handOverRequest(){
+        return null;
+    }
+    public String receiveRequest(Request request){
+        return null;
+    }
+
+    private String changeAvailability(Boolean status){
+        return null;
+    }
+
+    public String writeReview(){
+        return null;
+    }
+
+    public String updateReview(){
+        return null;
+    }
+
+    public String updateProfile(){
+        return null;
+    }
+
+    // Constructor
+    public JobSeeker() {
+    }
+
+    public JobSeeker(Boolean openToWork, List<Review> reviews, List<Invoice> invoices, List<Integer> assignedJobAd) {
+        this.openToWork = openToWork;
+        this.reviews = reviews;
+        this.invoices = invoices;
+        this.assignedJobAd = assignedJobAd;
+    }
+
+    // Getters and setters
+    public Boolean getOpenToWork() {
+        return openToWork;
+    }
+
+    public void setOpenToWork(Boolean openToWork) {
+        this.openToWork = openToWork;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public List<Invoice> getInvoices() {
+        return invoices;
+    }
+
+    public void setInvoices(List<Invoice> invoices) {
+        this.invoices = invoices;
+    }
+
+    public List<Integer> getAssignedJobAd() {
+        return assignedJobAd;
+    }
+
+    public void setAssignedJobAd(List<Integer> assignedJobAd) {
+        this.assignedJobAd = assignedJobAd;
+    }
 }
