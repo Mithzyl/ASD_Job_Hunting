04da1a45dccac8fabbc40d63c9b7f6ed073ba169
Alexis <517711766@qq.com>
2022-12-05 07:13:29 +0800

    Implemented Database

---
+++ src/Database/Database.java
@@ -0,0 +1,17 @@
+package Database;
+
+import java.util.List;
+
+public class Database {
+    private List<String> tableNames;
+
+    public void persist(){}
+
+    public void makeConnection(String userName, String password){}
+
+    public void fetch(){}
+
+    public void update(){}
+
+    public void delete(){}
+}
---
+++ src/Database/DatabaseConnector.java
@@ -0,0 +1,28 @@
+package Database;
+
+public class DatabaseConnector {
+    private String connection;
+
+    private String url;
+
+    private String loginUser;
+
+    private String loginPassword;
+
+    public void createSqlStatement(String statement){}
+
+    public void getConnection(String userName, String password){}
+
+    public void closeConnection(String userName, String password){}
+
+    public void executeSqlStatement(String statement){}
+
+    public void commit(){}
+
+    public void rollback(){}
+
+
+    // constructor
+    public DatabaseConnector() {
+    }
+}
