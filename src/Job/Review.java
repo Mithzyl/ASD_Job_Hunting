f44ea58464da5c8c581f42fe81b027f42b3501bb
Alexis <517711766@qq.com>
2022-12-05 07:26:44 +0800

    Implemented Review

--- src/Job/Review.java
+++ src/Job/Review.java
@@ -1,4 +1,20 @@
 package Job;
 
 public class Review {
+    public String reviewId;
+
+    public String jobId;
+
+    public String reviewedById;
+
+    public String reviewedToId;
+
+    public void writeReview(){}
+
+    public void updateReview(){}
+
+
+    // constructor
+    public Review() {
+    }
 }
