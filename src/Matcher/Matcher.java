c71bb0d1bb3d2c591953c51beccceda11d539120
Alexis <517711766@qq.com>
2022-12-05 06:18:25 +0800

    Implemented Matcher

---
+++ src/Matcher/Matcher.java
@@ -0,0 +1,25 @@
+package Matcher;
+
+import Job.JobAd;
+import JobSeeker.JobSeeker;
+import Server.Request;
+
+import java.util.List;
+
+public class Matcher {
+    public List<Integer> jobSeekers;
+
+    public List<Integer> jobAds;
+
+    public List<Integer> match(JobAd jobAd){return null;}
+
+    public void invite(Integer jobSeekerId){}
+
+    private void executeRequest(){}
+
+    private void handOverRequest(Request request){}
+
+    public void receiveRequest(Request request){}
+
+    public void getUnassignedJobAds(){}
+}
